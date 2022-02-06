b3bbab29477ba2eed44182cbf4acf168cd9d07a4
elsuppo <andrew.suponev@gmail.com>
2022-02-06 14:37:11 +0300

    feat: add play-pause function

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,1 +1,24 @@
-console.log('Audio player');
+const audioPlayer = document.querySelector('.app');
+const audio = audioPlayer.querySelector('.audio');
+const buttonPlay = audioPlayer.querySelector('.button-play');
+const cover = audioPlayer.querySelector('.song-info__cover');
+
+
+let playStatus = false;
+
+// Play and pause audio
+function playPauseAudio() {
+    if (playStatus === false) {
+        buttonPlay.className = "button button-pause";
+        audio.play();
+        cover.classList.add("playing");
+        playStatus = true;
+    } else {
+        buttonPlay.className = "button button-play";
+        audio.pause();
+        cover.classList.remove("playing");
+        playStatus = false;
+    }
+}
+
+buttonPlay.addEventListener('click', playPauseAudio);
