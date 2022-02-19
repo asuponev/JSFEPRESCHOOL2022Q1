f1e70e05ce9915737ca89906785399b619e72b91
elsuppo <andrew.suponev@gmail.com>
2022-02-19 23:46:47 +0300

    feat: draw snake,game over and  fix move

--- game-snake/js/index.js
+++ game-snake/js/index.js
@@ -1,12 +1,26 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const scoreValue = document.querySelector('.score-value')
 
 let speed = 7;
 
+let score = 0;
+
 let cellCount = 20;
 let cellSize = canvas.width / cellCount
-let xHeadSnake = Math.floor(Math.random() * cellCount);
-let yHeadSnake = Math.floor(Math.random() * cellCount);
+
+let snake = []
+snake[0] = {
+    x: Math.floor(Math.random() * cellCount),
+    y: Math.floor(Math.random() * cellCount)
+}
+
+let xHeadSnake = snake[0].x;
+let yHeadSnake = snake[0].y;
+
+const sound = new Audio('./assets/audio/bite.mp3');
+sound.volume = 0.1;
+
 
 let xFood = Math.floor(Math.random() * cellCount);
 let yFood = Math.floor(Math.random() * cellCount);
@@ -14,29 +28,53 @@ let yFood = Math.floor(Math.random() * cellCount);
 let xDirection = 0;
 let yDirection = 0;
 
+let gameOver = false;
+
 drawGame()
 function drawGame() {
-    clearScreen();
-    moveSnake();
-    drawSnake();
+    if (gameOver) {
+        ctx.fillStyle = 'white';
+        ctx.font = '50px Arial'
+        ctx.fillText('GAME OVER!', canvas.width / 8.5, canvas.height / 2)
+        return
+    }
+    
+    clearScreen();   
     drawFood();
-    checkIntersection();
+    drawSnake();
+    moveSnake();
     setTimeout(drawGame, 1000 / speed);
 }
 
 function clearScreen() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(0,0,canvas.width,canvas.height)
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
 function drawSnake() {
-    ctx.fillStyle = 'orange'
-    ctx.fillRect(xHeadSnake * cellCount, yHeadSnake * cellCount, cellSize, cellSize)
-}
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snake[i].x * cellCount, snake[i].y * cellCount, cellSize, cellSize);
+    };
 
-function moveSnake() {
-    xHeadSnake = xHeadSnake + xDirection;
-    yHeadSnake = yHeadSnake + yDirection;
+    if (xFood == xHeadSnake && yFood == yHeadSnake) {
+        score++;
+        scoreValue.innerHTML = `Score: ${score}`
+        sound.play();
+        xFood = Math.floor(Math.random() * cellCount);
+        yFood = Math.floor(Math.random() * cellCount);
+    } else {
+        snake.pop();
+    };
+
+    let newHead = {
+        x: xHeadSnake,
+        y: yHeadSnake
+    };
+
+    checkSelfIntersection(newHead, snake);
+
+    snake.unshift(newHead);
 }
 
 function drawFood() {
@@ -44,10 +82,17 @@ function drawFood() {
     ctx.fillRect(xFood * cellCount, yFood * cellCount, cellSize, cellSize)
 }
 
-function checkIntersection() {
-    if (xFood == xHeadSnake && yFood == yHeadSnake) {
-        xFood = Math.floor(Math.random() * cellCount);
-        yFood = Math.floor(Math.random() * cellCount);
+function moveSnake() {
+    xHeadSnake += xDirection;
+    yHeadSnake += yDirection;
+}
+
+function checkSelfIntersection(head, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (head.x == arr[i].x && head.y == arr[i].y) {
+            gameOver = true;
+            break;
+        }
     }
 }
 
