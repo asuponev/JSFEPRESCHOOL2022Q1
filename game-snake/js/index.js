ec3e0359b97c6fee655cf3915d7b9d673cce73c9
elsuppo <andrew.suponev@gmail.com>
2022-02-19 17:59:06 +0300

    feat: move snake

--- game-snake/js/index.js
+++ game-snake/js/index.js
@@ -1,31 +1,60 @@
-const canvas = document.getElementById('game')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-const ground = new Image()
-ground.src = './assets/img/ground.png'
+let speed = 7;
 
-const food = new Image()
-food.src = './assets/img/food.png'
+let cellCount = 20;
+let cellSize = canvas.width / cellCount
+let xHeadSnake = Math.floor(Math.random() * cellSize);
+let yHeadSnake = Math.floor(Math.random() * cellSize);
 
-let box = 32
+let xDirection = 0;
+let yDirection = 0;
 
-let score = 0
-
-let foodCoordinate = {
-    x: Math.floor((Math.random() * 17 + 1)) * box,
-    y: Math.floor((Math.random() * 15 + 1)) * box,
-}
-
-let snake = []
-snake[0] = {
-    x: 9 * box,
-    y: 10 * box
-}
-
+drawGame()
 function drawGame() {
-    ctx.drawImage(ground, 0, 0)
+    clearScreen();
+    moveSnake();
+    draSnake();
 
-    ctx.drawImage(food, foodCoordinate.x, foodCoordinate.y)
+    setTimeout(drawGame, 1000 / speed)
 }
 
-let game = setInterval(drawGame, 100)
+function clearScreen() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+}
+
+function draSnake() {
+    ctx.fillStyle = 'orange'
+    ctx.fillRect(xHeadSnake * cellCount, yHeadSnake * cellCount, cellSize, cellSize)
+}
+
+function moveSnake() {
+    xHeadSnake = xHeadSnake + xDirection;
+    yHeadSnake = yHeadSnake + yDirection;
+}
+
+document.body.addEventListener('keydown', keyDown)
+
+function keyDown(event) {
+    if (event.keyCode == 37 && xDirection != 1) {
+        yDirection = 0;
+        xDirection = -1;
+    }
+
+    if (event.keyCode == 38 && yDirection != 1) {
+        yDirection = -1;
+        xDirection = 0;
+    }
+
+    if (event.keyCode == 39 && xDirection != -1) {
+        yDirection = 0;
+        xDirection = 1;
+    }
+
+    if (event.keyCode == 40 && yDirection != -1) {
+        yDirection = 1;
+        xDirection = 0;
+    }
+}
